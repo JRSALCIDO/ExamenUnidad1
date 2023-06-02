e14caf32043c172380a31acf870547e633544df5
JRSALCIDO <salcido.12311@gmail.com>
2023-06-02 12:57:06 -0700

    Se programo el mainActtivity

--- app/src/main/java/com/example/examenunidad1/CuentaBanco.java
+++ app/src/main/java/com/example/examenunidad1/CuentaBanco.java
@@ -10,5 +10,7 @@ public class CuentaBanco extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuenta_banco);
+
+
     }
 }
--- app/src/main/java/com/example/examenunidad1/MainActivity.java
+++ app/src/main/java/com/example/examenunidad1/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.examenunidad1;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private EditText num, nombre, banco, saldo;
+    private Button enviar, salir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        num = findViewById(R.id.num);
+        nombre = findViewById(R.id.nombre);
+        banco = findViewById(R.id.banco);
+        saldo = findViewById(R.id.saldo);
+        enviar = findViewById(R.id.Enviar);
+        salir = findViewById(R.id.salir);
+
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, CuentaBanco.class);
+
+                intent.putExtra("num", num.getText().toString());
+                intent.putExtra("nombre", nombre.getText().toString());
+                intent.putExtra("banco", banco.getText().toString());
+                intent.putExtra("saldo", saldo.getText().toString());
+
+                startActivity(intent);
+            }
+        });
+
+        salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Confirmar salida")
+                        .setMessage("¿Estás seguro de que quieres salir?")
+                        .setNegativeButton(android.R.string.no, null)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface arg0, int arg1) {
+                                MainActivity.super.onBackPressed();
+                            }
+                        }).create().show();
+            }
+        });
+
     }
 }
